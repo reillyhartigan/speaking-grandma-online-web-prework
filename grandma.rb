aaeb31f16276fa6d0c80aed8ed27a795d2684d5e
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-03-31 16:13:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
 
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
